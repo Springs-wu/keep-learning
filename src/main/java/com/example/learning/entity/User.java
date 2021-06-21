960382371303c6fe32e0c4d0399a730c501e39dd
Springs-wu <yywuzc55@qq.com>
2021-06-21 13:42:01 +0800

    引入jol 测试对象字节大小

---
+++ src/main/java/com/example/learning/entity/User.java
@@ -0,0 +1,10 @@
+package com.example.learning.entity;
+
+public class User {
+    private Integer age;
+    //private String name;
+    private char sex;
+    private boolean flag;
+    private byte aByte;
+    private byte aByte2;
+}
--- src/main/java/com/example/learning/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/learning/service/impl/UserServiceImpl.java
@@ -1,13 +1,12 @@
 package com.example.learning.service.impl;
 
 import com.alibaba.fastjson.JSON;
+import com.example.learning.entity.User;
 import com.example.learning.helper.RedisHelper;
 import com.example.learning.service.UserService;
+import org.openjdk.jol.info.ClassLayout;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.Resource;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -21,5 +20,12 @@ public class UserServiceImpl implements UserService {
         String s = JSON.toJSONString(a);
         redisHelper.set("user", s);
         return false;
+    }
+
+    public static void main(String[] args) {
+        User user = new User();
+        User[] users = new User[10];
+        System.out.println(ClassLayout.parseInstance(user).toPrintable());
+        System.out.println(ClassLayout.parseInstance(users).toPrintable());
     }
 }
